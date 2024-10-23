849d82cf1d716bbf0fe7c5d35223209136dca042
mistahbrown <zachary.brown@worcesteracademy.org>
2024-10-23 11:45:14 -0400

    "PSET2"

---
+++ problems.h
@@ -0,0 +1,34 @@
+#include "stack.h"
+using namespace std;
+
+// Takes in an integer num and reverses it using a Stack data structure.
+// reverseInt(6883) should return 3886
+// reverseInt(0) should return 0
+// reverseInt(-2938947) should return -7498392
+int reverseInt(int num)
+{
+    // Your code here
+}
+
+// Returns -1 if celebrity
+// is not present. If present,
+// returns id (value from 0 to n-1).
+int findCelebrity(int n, vector<vector<int> >& matrix)
+{   
+    // Your code here
+}
+
+
+// Moves a disk from stack a to stack b if possible, otherwise
+// moves disk from stack b to stack a.
+void moveDisk(int a, int b)
+{
+    // Your code here
+}
+ 
+// Simulates the tower of Hanoi puzzle for n disks and solves it 
+// using the moveDisk() fuunction. No iteration allowed!
+void towerOfHanoi(int n)
+{
+    // Your code here
+}
---
+++ stack.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Node
+{
+    // Your code here
+};
+
+class Stack
+{
+    // Your code here
+};
